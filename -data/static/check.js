7d276660aa845f14beef6db231a7314b0192e444
badmojr <40524316+badmojr@users.noreply.github.com>
2022-06-16 06:58:00 +0300

    improve wording.. check text!
    Former-commit-id: 2850035d418f9df079100969c31c160d3b899b29

--- -data/static/check.js
+++ -data/static/check.js
@@ -3,6 +3,6 @@
 		var c=document.getElementById('doTest');
 		var i=document.createElement('img');
 		i.src='https://e6.pages.dev/img/x.png?_='+Date.now();
-		i.onload=function(){c.innerHTML = "<span class='text-danger'>Inactive</span>";};
-		i.onerror=function(){c.innerHTML = "<span class='text-success'>Active</span>";};
+		i.onload=function(){c.innerHTML = "<span class='text-danger'>Unable to check!</span>";};
+		i.onerror=function(){c.innerHTML = "<span class='text-success'>Enabled</span>";};
 		})();
